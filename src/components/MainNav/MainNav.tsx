b1feecd764d376ac5e351b3b47533c066a7ad250
Josh <jsbusiness305@gmail.com>
2022-10-13 17:20:50 -0400

    added icons to main nav

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { AiOutlineCar } from 'react-icons/ai';
 import {Link} from 'react-router-dom';
 import './MainNav.css';
 
 function MainNav() {
   return (
     <nav className='main_nav'>
-        <div><Link to='/'>Carvana</Link></div>
+        <div className='brand'><Link to='/'><AiOutlineCar/>Carvana</Link></div>
         <div><Link to='/cars'>Search Cars</Link></div>
         
     </nav>
